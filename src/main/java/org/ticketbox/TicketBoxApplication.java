6410d038b3bd6ad41d4b8bc3ab96373583b3a7e6
PhamCongMinh <minhctthvn@gmail.com>
2025-03-15 12:45:46 +0700

    feat: register booking service with eureka server

--- src/main/java/org/ticketbox/TicketBoxApplication.java
+++ src/main/java/org/ticketbox/TicketBoxApplication.java
@@ -3,12 +3,14 @@ package org.ticketbox;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
 @EnableCaching
+@EnableDiscoveryClient
 public class TicketBoxApplication {
 
     public static void main(String[] args) {
